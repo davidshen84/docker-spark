2daa049b1714e4bc0b72604f79182b82c3f63bf4
Xi Shen <davidshen84@gmail.com>
2018-10-06 20:52:31 +0800

    Add start/stop function to startup.sh.

--- root/startup.sh
+++ root/startup.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
-role=${1:-master}
+set -m
 
-if [ "$role" = master ]; then
+role=${1:-master}
+PID=
+
+start() {
+  if [ "$role" = master ]; then
     echo "Starting Spark master!"
     /opt/spark/sbin/start-master.sh
-elif [ "$role" = slave ]; then
+  elif [ "$role" = slave ]; then
     echo "Starting Spark slave!"
     /opt/spark/sbin/start-slave.sh spark://spark-master:7077
-else
-  echo "Value $role is unknown!!!"
-fi
+  else
+    echo "Value $role is unknown!!!"
+  fi
 
-tail -f /opt/spark/logs/*
+  tail -f /data/logs/* &
+  PID=$!
+}
+
+stop() {
+  if [ "$role" = master ]; then
+    echo "Stopping Spark master!"
+    /opt/spark/sbin/stop-master.sh
+  elif [ "$role" = slave ]; then
+    echo "Stopping Spark slave!"
+    /opt/spark/sbin/stop-slave.sh
+  else
+    echo "Value $role is unknown!!!"
+  fi
+
+  kill -TERM $PID
+  exit 0
+}
+
+trap stop SIGTERM SIGINT
+start
+wait $PID
+
